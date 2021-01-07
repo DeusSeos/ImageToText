9d885425d63459fc3cafb2120e2c3f2843e3041b
DeusSeos <45889782+DeusSeos@users.noreply.github.com>
2021-01-07 13:51:36 -0800

    Multiprocessing test finished

--- multiprocessingtest.py
+++ multiprocessingtest.py
@@ -1,34 +1,52 @@
 import multiprocessing as mp
 import queue
+import time
 
-def function(l, fileName):
-    l.acquire()
-    print(fileName + ".png")
-    l.release()
+
+def function(data, ret):
+    # l.acquire()
+    time.sleep(1)
+    print(data[1] + ".png")
+    ret.put((data[0], data[1]+ ".png"))
+    # l.release()
 
 
 if __name__ == '__main__':
-    lock = mp.Lock()
+    # lock = mp.Lock()
     core_count = mp.cpu_count()
     print(core_count)
     fileList = ["One", "Two", "Three", "Four", "Five"]
     workQueue = queue.Queue(10)
+    results = mp.Queue()
     processes = []
-    values = []
+    orderID = 0
+
+    start = time.perf_counter()
+
 
     for file in fileList:
-        workQueue.put(file)
+        workQueue.put((orderID, file))
+        orderID += 1
 
     while not workQueue.empty():
         data = workQueue.get()
-        p = mp.Process(target=function, args=(lock, data))
+        p = mp.Process(target=function, args=(data,results))
         p.start()
         processes.append(p)
 
     for p in processes:
         p.join()
 
-    for v in values:
-        print(v.value)
+    content = []
+
+    while not results.empty():
+        value = results.get()
+        content.insert(value[0], value[1])
+
+
+
+    end = time.perf_counter()
+
+    print(f"Finished in {end-start} second(s)")
 
     print("Done")
